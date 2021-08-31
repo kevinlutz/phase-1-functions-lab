a45ab227fc5074d349f55ff2beff0a187e309858
+ <+>
2021-08-31 17:28:23 -0400

    commit tree 0f21fa85fb21359231c84ff6eb35019520bcf6d6

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  if (blocks > 42) {
+    return blocks - 42;
+  } else if (blocks < 42) {
+    return 42 - blocks;
+  } else {
+    return 0;
+  }
+}
+
+distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(blocks) {
+  return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(num1, num2) {
+  return (num2 - num1) * 264;
+}
